cf348b713c337d37155c8dd362f1ee37c2951156
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-10-17 23:57:12 +0900

    [level 1] Title: 최소직사각형, Time: 8.34 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,4 +1,28 @@
 function solution(sizes) {
-    var answer = 0;
-    return answer;
+    let longerArr = [];
+    let shorterArr = [];
+    let longest;
+    let shortest;
+    
+    // sizes 의 배열 item을 [작,큰]으로 정렬
+    for(let i=0; i<sizes.length; i++) {
+        sizes[i].sort((a,b) => b - a);
+    }
+    
+    // item 중에 더 큰 요소 bigger 배열 / 작은 요소 shorter 배열에 넣어주기
+    sizes.forEach((item) => {
+        if(item[0] > item[1]) {
+            longerArr.push(item[0]);
+            shorterArr.push(item[1]);
+        } else {
+            longerArr.push(item[1]);
+            shorterArr.push(item[0]);
+        }
+    });
+    
+    // 긴 배열, 작은 배열 중 제일 큰 수 추출
+    longest = Math.max(...longerArr);
+    shortest = Math.max(...shorterArr);
+    
+    return longest * shortest;
 }
