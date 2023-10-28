42fd58c79b16f51e5b086695abcf9a0c35d24be6
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-10-28 17:48:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.66 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,7 +2,6 @@ function solution(numbers) {
     let answer = [];
     
     // 서로 다른 index에 있는 두 수 더해서 배열 만들기
-    // 오름차순 정렬
     for(let i=0; i<numbers.length; i++){
         for(let j=i+1; j<numbers.length; j++){
             answer.push(numbers[i] + numbers[j]);
