991807024a2ba3f585584ef0999a1fd4a57ebb04
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-11-06 02:22:51 +0900

    [level 2] Title: 올바른 괄호, Time: 4.14 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -5,19 +5,20 @@ function solution(s){
     // 문자열 s를 배열로 바꾸기
     s = s.split("");
     
+    // s 배열의 길이가 1 or 닫는괄호로 시작 시, false return
     if(s.length === 1 || s[0] === ")") return false;
     
     // s에서 '('인 경우 +1 , ')'인 경우 -1해서 합이 0이면 true, 아니면 false
     s.forEach((item, idx) => {
-        // 첫번째 요소는 무조건 여는 괄호
         if(item === "(") {
             sScore++;
         }
         else {
-         if(sScore < 1) {
-             answer = false;
-             return false;
-         }
+            // 여는 괄호가 없는 경우 false
+            if(sScore < 1) {
+                 answer = false;
+                 return false;
+            }
         sScore--;
         }
     });
