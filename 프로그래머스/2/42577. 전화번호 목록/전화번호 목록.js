ee7028b009bf09d35de33d597be39cc1081c8e30
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-12-03 23:23:33 +0900

    [level 2] Title: 전화번호 목록, Time: 386.94 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -4,15 +4,19 @@ function solution(phone_book) {
     // * 하나라도 접두어인지 확인
     // 한 전화번호가 다른 번호의 접두어라면 F, 아니면 T 반환
     
-    // 1. phone_book 정렬
-    phone_book.sort();
+    // 1. HashMap 생성
+    const table = {};
+    for (const number of phone_book) {
+        table[number] = true;
+    };
     
-    // 2. 1중 loop를 돌면서 앞번호가 뒷번호의 접두어인지 확인 함
-    for(let i=0; i<phone_book.length - 1; i++){
-        if(phone_book[i+1].startsWith(phone_book[i])) {
-            answer = false;
-        }
-    }
+    // 2. 모든 전화번호의 접두어가 Hash에 있는 지 확인
+    for (const number of phone_book) {
+        for (let i = 1; i < number.length; i++) {
+          const prefix = number.slice(0, i);
+          if (table[prefix]) return false;  
+        };
+    };
     
     // 3. 여기까지 오지 못했다면 접두어가 없는 것
     return answer;
