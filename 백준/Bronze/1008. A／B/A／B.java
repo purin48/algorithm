089625c6a4f86b03b2b28984f9cd824436def453
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-04 18:16:56 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextFloat();
+        double b = sc.nextFloat();
+        System.out.println(a / b);
+    }
+}
