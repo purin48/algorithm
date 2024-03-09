028019f356ee99a6af2354abdd3592f3c4974864
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-10 04:23:50 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        String result = "";
+        if(score >= 90) result = "A";
+        else if (score >= 80) result = "B";
+        else if (score >= 70) result = "C";
+        else if (score >= 60) result = "D";
+        else result = "F";
+        System.out.println(result);
+    }
+}
