fb0861e6da8cb67dc26cfe4fc4cbbb51693591a5
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-18 02:53:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 1776 ms, Memory: 319660 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0];
+        int max = arr[0];
+        for(int i=0; i<n; i++) {
+            if(min > arr[i]) min = arr[i];
+            else if (max < arr[i]) max = arr[i];
+        }
+        System.out.println(min + " " + max);
+    }
+}
