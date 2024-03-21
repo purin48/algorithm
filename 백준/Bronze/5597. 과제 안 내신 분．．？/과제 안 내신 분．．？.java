ad4c6aa46277ae34842208c2df35c8086f632e2f
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-21 20:55:23 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 212 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[] arr = new boolean[30];
+        for(int i=0; i<28; i++) {
+            int num = sc.nextInt()-1;
+            arr[num] = true;
+        }
+        for(int i=0; i<arr.length; i++) {
+            if(!arr[i]) System.out.println(i+1);
+        }
+    }
+}
