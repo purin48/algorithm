0d45b539fcabc08c9922bacffe0c55f3bb0f4515
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-22 13:45:44 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17568 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,30 +1,15 @@
-// 백준 제출 전 package 주석처리
-import java.util.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-public class Main { 
-    public static void main(String[] n) {
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // boolean값으로 채워진 42길이의 배열 생성
-        boolean[] visited = new boolean[42];
+        Set<Integer> arr = new HashSet<Integer>();
 
-        int count = 0;
-        // 숫자 10번 입력 받기
-        for (int i=0; i<10; i++) {
-            int num = sc.nextInt();
-
-            // 42로 나눈 나머지를 구하기
-            int remain = num % 42;
-
-            if(visited[remain]) {
-                // 이미 기록된 나머지이기 때문에 뒤에 작업하지 X
-                continue; 
-            }
-            if(!visited[remain]) {
-                // 중복이 없는 수는 true로 변경
-                visited[remain] = true;
-                count++;
-            }
+        for(int i=0; i<10; i++) {
+            arr.add(sc.nextInt() % 42);
         }
-        System.out.println(count);
+        System.out.println(arr.size());
     }
 }
